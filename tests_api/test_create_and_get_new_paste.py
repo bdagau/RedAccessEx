646e6c02ccddb48b2e2abac2c30cd6d25f05c860
bdagau <45761119+bdagau@users.noreply.github.com>
2023-02-05 10:17:50 +0200

    tests for red access ex

--- tests_api/test_create_and_get_new_paste.py
+++ tests_api/test_create_and_get_new_paste.py
@@ -3,4 +3,6 @@ from tests_api.pastebin_api_handler.pastebin_api_handler import PastebinApiHandl
 
 def test_create_and_get_new_paste():
     api_handler = PastebinApiHandler()
-    api_handler.create_new_paste("test")
+    res = api_handler.create_new_paste("test")
+    assert res.status_code == 200
+
